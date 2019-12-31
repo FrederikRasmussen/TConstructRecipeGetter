de94e8d05053eebf10165cc1aab29abfc33b9300
Nuskatev <lundbyft@gmail.com>
2019-12-31 12:04:18 +0100

    Main mod file

---
+++ src/main/java/tconstructrecipegetter/TConstructRecipeGetter.java
@@ -0,0 +1,17 @@
+package tconstructrecipegetter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+    modid = TConstructRecipeGetter.MOD_ID,
+    name = TConstructRecipeGetter.NAME,
+    version = TConstructRecipeGetter.VERSION
+)
+public class TConstructRecipeGetter {
+    public static final String MOD_ID = 
+            "tconstructrecipegetter";
+    public static final String NAME =
+            "Tinkers' Construct Recipe Getter";
+    public static final String VERSION =
+            "@VERSION@";
+}
